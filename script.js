2c63cb80a5566ba6a3402a2e739bc4478dff0796
allie <alexandriasklenar@gmail.com>
2022-07-19 15:17:09 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function createBoard(size) {
+  return size;
+}
+
+function init() {
+  let container = document.querySelector(".drawing-container");
+  let singleSquare = document.createElement("div");
+}
+
+init();

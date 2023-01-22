6fb5f9f33f0de320582e7ded9e63aa908fe40787
allie <alexandriasklenar@gmail.com>
2023-01-21 19:43:17 -0800

    add erase mode

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const DEFAULT_GRID_SIZE = 16;
 let gridOn = true;
+let eraseModeOn = false;
 
 let currentSize = DEFAULT_GRID_SIZE;
 let drawingContainer = document.querySelector(".drawing-container");
 let slider = document.querySelector(".slider");
 let gridButton = document.querySelector(".grid-toggle");
 let clearCanvasButton = document.querySelector(".clear-canvas");
+let modeToggleButton = document.querySelector(".mode-toggle");
 
 function createBoard(size) {
   drawingContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -19,7 +21,11 @@ function createBoard(size) {
 }
 
 function onChange(singleSquare) {
-  singleSquare.classList.add("filled");
+  if (!eraseModeOn) {
+    singleSquare.classList.add("filled");
+  } else {
+    singleSquare.classList.remove("filled");
+  }
 }
 
 function sliderChange() {
@@ -48,10 +54,29 @@ function clearCanvas() {
   }
 }
 
+// function affectAllSquares(){
+//   let allSquares = document.querySelectorAll(".square");
+//   for (square of allSquares) {
+//     square.classList.remove("filled");
+//   }
+// }
+
+function modeToggle() {
+  if (eraseModeOn) {
+    modeToggleButton.textContent = "draw mode";
+    console.log(modeToggleButton.textContent);
+  } else {
+    modeToggleButton.textContent = "erase mode";
+    console.log(modeToggleButton.textContent);
+  }
+  eraseModeOn = !eraseModeOn;
+}
+
 function init() {
   slider.addEventListener("mouseup", sliderChange);
   gridButton.addEventListener("click", toggleGrid);
   clearCanvasButton.addEventListener("click", clearCanvas);
+  modeToggleButton.addEventListener("click", modeToggle);
   createBoard(currentSize);
   toggleGrid();
 }
